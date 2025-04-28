76efd12ed9428de9c449b0dfa9ed2f03a5697030
MoDiouf <mohameddiouf096@gmail.com>
2025-04-28 22:55:15 +0000

    onk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
+
+
