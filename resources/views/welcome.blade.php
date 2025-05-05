302fc3d5729b563e1c3aa37ebdb68bf0b1fd958b
MoDiouf <mohameddiouf096@gmail.com>
2025-05-05 11:07:10 +0000

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,248 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suniu Transfert</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            color: #333;
-            line-height: 1.6;
-            background-color: #f9f9f9;
-        }
-
-        header {
-            padding: 20px;
-            border-bottom: 1px solid #eee;
-            background-color: #242323;
-            color: white;
-        }
-
-        .logo {
-            font-size: 24px;
-            font-weight: bold;
-            margin-bottom: 10px;
-            color: #efe845;
-        }
-
-        nav ul {
-            display: flex;
-            list-style: none;
-            padding: 0;
-            margin: 0;
-        }
-
-        nav ul li {
-            margin-right: 20px;
-        }
-
-        .language-login {
-            display: flex;
-            justify-content: flex-end;
-            margin-top: -30px;
-            gap: 3%;
-        }
-
-        .hero {
-            text-align: center;
-            padding: 40px 20px;
-            background-color: #f9f9f9;
-        }
-
-        .features {
-            display: flex;
-            justify-content: space-around;
-            padding: 30px 0;
-            text-align: center;
-            background-color: #f9f9f9;
-        }
-
-        .feature {
-            width: 30%;
-        }
-
-        .form-container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: white;
-            width: 55%;
-        }
-
-        .form-group {
-            margin-bottom: 20px;
-        }
-
-        select,
-        input {
-            width: 100%;
-            padding: 10px;
-            margin-top: 5px;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-        }
-
-        .login {
-            border: #efe845 solid;
-            padding: 5px 15px;
-            text-decoration: none;
-            color: #efe845;
-        }
-        main{
-            display: flex;
-            padding: 10px;
-        }
-        .amount-display {
-            display: flex;
-            justify-content: space-between;
-            margin-top: 10px;
-        }
-
-        .amount-box {
-            width: 48%;
-            padding: 10px;
-            border: 1px solid #ddd;
-            text-align: center;
-            background-color: #f5f5f5;
-        }
-
-        .option-group {
-            margin-bottom: 20px;
-        }
-
-        .option-title {
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
-
-        .options {
-            display: flex;
-            flex-wrap: wrap;
-        }
-
-        .option {
-            margin-right: 15px;
-            margin-bottom: 10px;
-        }
-
-        .continue-btn {
-            background-color: #efe845;
-            color: #242323;
-            border: none;
-            padding: 12px 30px;
-            font-size: 16px;
-            border-radius: 4px;
-            cursor: pointer;
-            margin-top: 20px;
-        }
-
-        .register {
-            color: #242323;
-            background: #efe845;
-            border: #efe845 solid;
-            padding: 5px 15px;
-            text-decoration: none
-        }
-
-
-    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./Cuj.css">
 </head>
-
 <body>
     <header>
         <div class="logo">Suniu Tranfert</div>
         <nav>
             <ul>
-                <li>Send money</li>
-                <li>Pick-up cash</li>
-                <li>Track transfer</li>
-                <li>Pay bills</li>
-                <li>Register</li>
-                <li>Support</li>
+                <li><a href="#">Send money</a></li>
+                <li><a href="#">Pick-up cash</a></li>
+                <li><a href="#">Track transfer</a></li>
+                <li><a href="#">Pay bills</a></li>
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Support</a></li>
             </ul>
         </nav>
         <div class="language-login">
-            <a href="" class="login">Login</a>
-            <a href="" class="register">Register</a>
+            <a href="#" class="a1">Login</a>
+            <a href="#" class="a2">Register</a>
         </div>
     </header>
+    
+    
+    <section class="hero">
+        <h1>The fast and trusted way to send money online</h1>
+        <p>Whether you need to send money to friends down the street or family across the globe, Eastern Union gets your funds there quickly and reliably.</p>
+    </section>
+    
+    <section class="features">
+        <div class="feature">
+            <ion-icon name="fastforward" class="fast"></ion-icon>
+            <h3>Fast</h3>
+            <p>Send money online to loved ones across the world.</p>
+        </div>
+        <div class="feature">
+            <ion-icon name="checkmark-circle-outline" class="safe"></ion-icon>
+            <h3>Safe</h3>
+            <p>Feel secure knowing we have sent over a billion.</p>
+        </div>
+        <div class="feature">
+            <ion-icon name="thumbs-up" class="thumb"></ion-icon>
+            <h3>Great Value</h3>
+            <p>Great rates, special offers, and no hidden fees.</p>
+        </div>
+    </section>
+    
+    <form class="form-container" action="/" method="post">
+        <h2>Send money online</h2>
 
-    <main>
-        <section class="hero-container">
-            <section class="hero">
-                <h1>The fast and trusted way to send money online</h1>
-                <p>Whether you need to send money to friends down the street or family across the globe, Eastern Union gets your funds there quickly and reliably.</p>
-            </section>
-
-            <section class="features">
-                <div class="feature">
-                    <h3>Fast</h3>
-                    <p>Send money online to loved ones across the world.</p>
-                </div>
-                <div class="feature">
-                    <h3>Safe</h3>
-                    <p>Feel secure knowing we have sent over a billion.</p>
-                </div>
-                <div class="feature">
-                    <h3>Great Value</h3>
-                    <p>Great rates, special offers, and no hidden fees.</p>
-                </div>
-            </section>
-        </section>
-
-
-        <section class="form-container">
-            <h2>Send money online</h2>
-
-            <div class="form-group">
-                <div class="option-title">Receiver's country</div>
-                <select>
-                    <option>Select country</option>
-                </select>
+        <div class="form-group">
+            <div class="option-title">Receiver's country</div>
+            <select>
+                <option>Select country</option>
+                <option value="Senegal">Senegal</option>
+                <option value="Canada">Canada</option>
+                <option value="Italie">Italie</option>
+                <option value="Espagne">Espagne</option>
+                <option value="Angleterre">Angleterre</option>
+                <option value="Allemagne">Allemagne</option>
+                <option value="Croatie">Croatie</option>
+            </select>
+        </div>
+        
+        <div class="form-group">
+            <div class="option-title">
+                <p>Send amount</p>
+                <p>Receive amount</p>
             </div>
-
-            <div class="form-group">
-                <div class="option-title">Send amount</div>
-                <div class="option-title">Receive amount</div>
-                <div class="amount-display">
-                    <div class="amount-box">0.00</div>
-                    <div class="amount-box">0.00</div>
+            <div class="amount-display">
+                <input type="text" class="amount-box" value="0.00">
+                <ion-icon name="repeat" class="arrow"></ion-icon>
+                <input type="text" class="amount-box" value="0.00">
+            </div>
+        </div>
+        
+        <div class="option-group">
+            <div class="option-title">How does your receiver want the money?</div>
+            
+            <div class="options">
+                <div class="option">
+                    <img src="./wave.png" alt="Wave" height="40px">
+                    <p>Wave</p>
+                </div>
+                <div class="option">
+                    <img src="./OM.png" alt="Wave" height="40px">
+                    <p>Orange Money</p>
+                </div>
+                <div class="option">
+                    <img src="./FreeMoney.png" alt="Wave" height="40px">
+                    <p>Free Money</p>
                 </div>
             </div>
+            
+        </div>
+        
+        
+        <div class="option-title">How would you like to pay?</div>
+            <input type="radio" id="r1" name="choice"> 
+            <label for="r1">Pay online</label>
+            <input type="radio" id="r2" name="choice"> 
+            <label for="r2">Pay cash in-store</label>
+        
+        <div class="option-group">
+            <div class="option-title"></div>
 
-            <div class="option-group">
-                <div class="option-title">How does your receiver want the money?</div>
-                <div class="options">
-                    <div class="option">Cash pick-up</div>
-                    <div class="option">Bank account</div>
-                    <div class="option">Pay off</div>
+            <div class="options">
+                <div class="option">
+                    <img src="./wave.png" alt="Wave" height="40px">
+                    <p>Wave</p>
+                </div>
+                <div class="option">
+                    <img src="./OM.png" alt="Wave" height="40px">
+                    <p>Orange Money</p>
+                </div>
+                <div class="option">
+                    <img src="./FreeMoney.png" alt="Wave" height="40px">
+                    <p>Free Money</p>
                 </div>
             </div>
+            
+        </div>
+        
+        <button class="continue-btn">Continue ></button>
+    </form>
 
-            <div class="option-group">
-                <div class="option-title">How would you like to pay?</div>
-                <div class="options">
-                    <div class="option">Pay online</div>
-                    <div class="option">Pay cash in-store</div>
-                </div>
-            </div>
-
-            <div class="option-group">
-                <div class="option-title"></div>
-                <div class="options">
-                    <div class="option">Credit card</div>
-                    <div class="option">Debit card</div>
-                    <div class="option">Bank account</div>
-                </div>
-            </div>
-
-            <button class="continue-btn">Continue ></button>
-        </section>
-    </main>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 </body>
-
 </html>
